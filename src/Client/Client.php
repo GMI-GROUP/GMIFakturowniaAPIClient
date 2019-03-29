fceafaa7efc96b946d68554cdb5c40c469f23f0c
Paweł Moliński <p.molinski@gmi.pl>
2019-03-29 12:44:37 +0100

    Adds example class. Adds .env file. Adds .gitlab-ci.yml.

---
+++ src/Client/Client.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Client;
+
+class Client
+{
+
+}
---
+++ tests/Client/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Client;
+
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
